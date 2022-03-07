1033f687b84dab7fa83c0d958836525773b0ed82
Giovani Pereira <giovanips.dev@gmail.com>
2022-03-07 19:27:21 -0300

    adicionando operacoes aritimeticas

---
+++ bloco4-js-logica/exercicioJS.js
@@ -0,0 +1,28 @@
+const a = 8;
+const b = 2;
+
+// Adição //
+
+let soma;
+soma = a + b;
+console.log(soma);
+
+// Subtração //
+
+let subtracao = a - b;
+console.log(subtracao);
+
+// Multiplicação //
+
+let multiplicacao = a * b;
+console.log(multiplicacao);
+
+// Divisão //
+
+let divisao = a / b;
+console.log(divisao);
+
+// Módulo //
+
+let resto = a % b;
+console.log(resto);
