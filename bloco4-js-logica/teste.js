348d103eabdab65356fafa8e2d239b23b76fe73d
Giovani Pereira <giovanips.dev@gmail.com>
2022-03-07 18:30:42 -0300

    adicionando teste js

---
+++ bloco4-js-logica/teste.js
@@ -0,0 +1,15 @@
+/*
+const notaPessoa = 90;
+
+if(notaPessoa >= 80) {
+  console.log('Parabéns, você foi aprovada(o)!')
+} 
+
+else if(notaPessoa < 80 && notaPessoa >= 60){
+  console.log("Você está na nossa lista de espera")
+
+}else{
+  console.log("Você foi reprovada(o)")
+}
+*/
+
