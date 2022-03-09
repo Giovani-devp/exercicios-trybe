1f16b409a3be4e08a737b75cd4ad9a0c2cbc4d91
Giovani Pereira <giovanips.dev@gmail.com>
2022-03-09 20:33:16 -0300

    correçao media aritmetica

--- bloco4-js-logica/arrayEfor.js
+++ bloco4-js-logica/arrayEfor.js
@@ -1,10 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
-let mediaAritmetica = 0;
+let listaNumbers = numbers.length;
+let mediaAritmetica;
 
-for(index = 0; index < numbers.length; index++){    
-    soma += numbers[index]
-    mediaAritmetica = soma / soma;
+for(let i = 0; i < numbers.length; i++){
+    soma += numbers[i]
+    // console.log(soma);
+    mediaAritmetica = 278 / listaNumbers;
+}
 console.log(mediaAritmetica);
-
-} 
