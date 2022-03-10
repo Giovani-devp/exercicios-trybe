ae9ec11297a8c63538c7691e655d47a46ddf3332
Giovani Pereira <giovanips.dev@gmail.com>
2022-03-09 21:08:29 -0300

    maior numero

--- bloco4-js-logica/arrayEfor.js
+++ bloco4-js-logica/arrayEfor.js
@@ -3,9 +3,33 @@ let soma = 0;
 let listaNumbers = numbers.length;
 let mediaAritmetica;
 
-for(let i = 0; i < numbers.length; i++){
-    soma += numbers[i]
-    // console.log(soma);
-    mediaAritmetica = 278 / listaNumbers;
+for (let i = 0; i < numbers.length; i++) {
+  soma += numbers[i];
+  // console.log(soma);
+  mediaAritmetica = 278 / listaNumbers;
 }
 console.log(mediaAritmetica);
+
+if (mediaAritmetica > 20) {
+  console.log("valor maior que 20");
+} else {
+  console.log("valor menor ou igual a 20");
+}
+
+let maiorValor = 0;
+maiorValor = numbers[0];
+
+for (let index = 1; index < numbers.length; index++) {
+  if (maiorValor < numbers[index]) {
+    maiorValor = numbers[index];
+
+  }
+}
+console.log('Maior valor: ',maiorValor);
+
+
+for(let indice = 0; indice < numbers.length; indice++){
+  if(numbers[indice] % 2 !== 0){
+  console.log(numbers[indice]);
+  }
+}
